89483ce717b04862c6762182b2ccd3dce7ee876a
CRCastells <cameroncastells@gmail.com>
2017-11-17 08:35:15 -0700

    chuck norris

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	joke:any;
+  category: any;
 
+  constructor(
+   public navCtrl: NavController,
+   public http : Http
+  ) {
+
+  }
+
+  ngOnInit() {
+  	this.http.get('https://api.icndb.com/jokes/random').subscribe(res => {
+      this.joke = res.json().value.joke;
+    });
+  }
+
+  gimmeJoke() {
+    this.http.get(`https://api.icndb.com/jokes/random?limitTo=[${this.category}]`).subscribe(res => {
+      this.joke = res.json().value.joke;
+    });
   }
 
 }
